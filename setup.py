949279ac467ccf4f20a64ecc51ee134fec6ea1a3
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-01-08 15:34:20 -0600

    Add subfolder inclusion

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'clara',
   packages = ['clara'], # this must be the same as the name above
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'Conversational chit-chat utility agent',
   author = 'Noah Huber-Feely',
   author_email = 'nhuberfeely@gmail.com',
   url = 'https://github.com/huberf/clara-pip', # use the URL to the github repo
   download_url = 'https://github.com/huberf/clara-pip/archive/1.0.0.tar.gz', # I'll explain this in a second
   keywords = ['clara', 'chat'], # arbitrary keywords
+  packages = ['.','utils'],
+  package_data={'utils':['*']},
   classifiers = [],
 )
