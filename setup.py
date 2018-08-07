2de38b5049edc3ae81bb8d85528ddcfae86c4a17
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-08-07 10:32:12 -0400

    [Version 1.2.0] add in procedural capability

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
   ],
   package_data={'clara/utils':['*']},
   scripts=['bin/clara'],
-  version = '1.1.2',
+  version = '1.2.0',
   description = 'Conversational chit-chat utility agent',
   author = 'Noah Huber-Feely',
   author_email = 'nhuberfeely@gmail.com',
   url = 'https://github.com/huberf/clara-pip', # use the URL to the github repo
-  download_url = 'https://github.com/huberf/clara-pip/archive/1.1.2.tar.gz',
+  download_url = 'https://github.com/huberf/clara-pip/archive/1.2.0.tar.gz',
   keywords = ['clara', 'chat'], # arbitrary keywords
   classifiers = [],
 )
