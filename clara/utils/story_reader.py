3b9f77d453be4b99323635a0053cd1e50da24f30
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-02-19 20:31:00 -0500

    [stories] fix some bugs in the script

--- clara/utils/story_reader.py
+++ clara/utils/story_reader.py
@@ -6,6 +6,7 @@ STORY_INDEX = {}
 def load_story(file_name):
     contents = json.loads(open(file_name).read())
     convos = recursive_build(contents, None)
+    return convos
 
 def recursive_build(json_cont, parent_id):
     to_return = []
@@ -25,6 +26,11 @@ def recursive_build(json_cont, parent_id):
         to_return += [this_convo]
         try:
             STORY_INDEX[json_cont['id']] = this_convo
-        for i in json_cont['next']:
-            to_return += recursive_build(i, this_id)
+        except:
+            pass
+        try:
+            for i in json_cont['next']:
+                to_return += recursive_build(i, this_id)
+        except:
+            pass # This line has ended
     return to_return
