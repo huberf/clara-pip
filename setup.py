2d5ae9fbc7a509b54e98a814bb6ee6c3fee63ed7
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-05-31 15:30:45 -0400

    update to version 1.4.0

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
   ],
   package_data={'clara/utils':['*']},
   scripts=['bin/clara'],
-  version = '1.3.1',
+  version = '1.4.0',
   description = 'Conversational chit-chat utility agent',
   author = 'Noah Huber-Feely',
   author_email = 'nhuberfeely@gmail.com',
   url = 'https://github.com/huberf/clara-pip', # use the URL to the github repo
-  download_url = 'https://github.com/huberf/clara-pip/archive/1.3.1.tar.gz',
+  download_url = 'https://github.com/huberf/clara-pip/archive/1.4.0.tar.gz',
   keywords = ['clara', 'chat'], # arbitrary keywords
   classifiers = [],
 )
